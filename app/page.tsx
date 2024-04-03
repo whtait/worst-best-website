df8c0f09dfe9802f933cbe20dcac88e172298bc1
Warren Tait <warrenhtait@gmail.com>
2024-04-03 15:35:41 -0500

    Revert the last commit. I don't need Redux for this project.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
-"use client";
 import { Header } from "@/components";
-import { store } from "@/store";
-import { Provider } from "react-redux";
 
 export default function Home() {
-  return (
-    <Provider store={store}>
-      <Header />
-    </Provider>
-  );
+  return <Header />;
 }
